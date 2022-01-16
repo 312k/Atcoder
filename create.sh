83974bdf131cbf694d9934ee7c93752f3d0d4d1d
312k <55316963+312k@users.noreply.github.com>
2022-01-16 20:39:31 +0900

    Make generator

---
+++ create.sh
@@ -0,0 +1,24 @@
+#!/usr/bin/bash
+
+echo "How many times have ABC held?"
+
+read input
+
+mkdir ABC/$input
+
+touch ABC/$input/link.md
+
+echo https://atcoder.jp/contests/abc$input/ > ABC/$input/link.md
+
+echo "What file do you want to make?"
+echo "When to finish you type end"
+while true
+do
+	read file_name
+	if [ $file_name = "end" ]; then
+  		exit
+	else
+		cp template/main.c ABC/$input/$file_name.c
+	fi
+done
+
