00a57cb30cbb72644dab8c593d9af2f6963ec671
312k <55316963+312k@users.noreply.github.com>
2022-01-16 20:22:47 +0900

    2nd b.c#

--- ABC/200/b.c
+++ ABC/200/b.c
@@ -2,8 +2,8 @@
 
 int main()
 {
-    unsigned long int N;
-    unsigned long int K;
+    unsigned  int N;
+    unsigned  int K;
     scanf("%d", &N);
     scanf("%d", &K);
     for (int i = 0; i < K; i++)
