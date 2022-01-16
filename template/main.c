f14b95ca35ed3587b4d9a5318aa7cf0490ecbd6a
312k <55316963+312k@users.noreply.github.com>
2022-01-16 21:12:21 +0900

    Add speed up temp

--- template/main.c
+++ template/main.c
@@ -1,4 +1,7 @@
 #include <stdio.h>
+#pragma GCC optimize("Ofast")
+#pragma GCC optimize("unroll-loops")
+#pragma GCC target("sse,sse2,sse3,ssse3,sse4,fma,abm,mmx,avx,avx2")
 
 int main(){
 	int a,b;
